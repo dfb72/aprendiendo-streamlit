de3257d6421f2ee32ec65b8971519cc99b32a525
dfb72 <163043829+dfb72@users.noreply.github.com>
2024-05-29 16:42:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ from collections import Counter
 # Cargar los datos
 @st.cache
 def cargar_datos('IMDB-Movie-Data.csv'):
-    return pd.read_csv('IMDB-Movie-Data.csv')
+    data = pd.read_csv('IMDB-Movie-Data.csv')
+    return data
 
 # Función de búsqueda
 def buscar_peliculas(df, titulo=None, genero=None, año=None):
